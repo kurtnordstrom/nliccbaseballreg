4ab3a92bffaae1f3f0ff236d970c7de9338818b9
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2026-02-10 22:55:12 -0500

    Add validation to registration form

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,11 +1,15 @@
 import {useState} from "react"
 import api from "../api"
 import {useNavigate} from "react-router-dom"
+import {checkDateString} from "../util"
 
 function RegisterForm({route}) {
     const [userObject, setUserObject] = useState({})
     const [personObject, setPersonObject] = useState({is_user: true})
     const [familyObject, setFamilyObject] = useState({registration_submitted: false})
+    const [passwordCheck, setPasswordCheck] = useState("")
+    const [submissionErrors, setSubmissionErrors] = useState([])
+    const [showErrorModal, setShowErrorModal] = useState(false)
 
     const navigate = useNavigate()
 
@@ -15,12 +19,45 @@ function RegisterForm({route}) {
         submissionObject.person = structuredClone(personObject);
         submissionObject.family = structuredClone(familyObject);
 
-        try {
-            const res = await api.post(route, submissionObject)
-            navigate("/login")
-        } catch (error) {
-            alert (error) 
-        } 
+        const errors = doValidation(submissionObject)
+
+        if (errors.length) {
+            setShowErrorModal(true)
+        } else {
+            try {
+                const res = await api.post(route, submissionObject)
+                navigate("/login")
+            } catch (error) {
+                alert (error) 
+            } 
+        }
+    }
+
+    const doValidation = (submissionObject) => {
+        const errors = []
+        if(!submissionObject.username) {
+            errors.push("You must provide username")
+        }        
+        if(!submissionObject.password) {
+            errors.push("You must provide a password")
+        }
+        if(submissionObject.password != passwordCheck) {
+            errors.push("Provided passwords do not match")
+        }
+        if(!submissionObject.person.first_name || !submissionObject.person.last_name) {
+            errors.push("You must provide a first and last name")
+        }
+        if(!submissionObject.person.email) {
+            errors.push("You must provide a valid email address")
+        }
+        if(!checkDateString(submissionObject.person.date_of_birth)) {
+            errors.push("You must provide a birthdate in the form of YYYY-MM-DD")
+        }
+        if(!submissionObject.family.family_name) {
+            errors.push("Please provide a name for your family (typically your last name)")
+        }
+        setSubmissionErrors(errors)
+        return errors
     }
 
     const handleUserChange = (e) => {
@@ -48,6 +85,7 @@ function RegisterForm({route}) {
     }
 
     return (
+        <div>
         <form onSubmit={handleSubmit} className="form-container">
             <h1>Register New User</h1>
             <label>Username</label>
@@ -66,13 +104,13 @@ function RegisterForm({route}) {
                 name="password"
                 onChange={handleUserChange}
             />
-            <label>Family Name</label>
+            <label>Password (Again)</label>
             <input
                 className="form-input"
-                type="text"
-                value={familyObject.family_name}
-                name="family_name"
-                onChange={handleFamilyChange}
+                type="password"
+                value={passwordCheck}
+                name="password-again"
+                onChange={(e) => setPasswordCheck(e.target.value)}
             />
             <label>First Name</label>
             <input
@@ -114,13 +152,39 @@ function RegisterForm({route}) {
                 name="address"
                 onChange={handlePersonChange}
             />
+            <label>Family Name</label>
+            <input
+                className="form-input"
+                type="text"
+                value={familyObject.family_name}
+                name="family_name"
+                onChange={handleFamilyChange}
+            />
             <button
                 className="form-button"
                 type="submit">
                     Submit
             </button>
         </form>
+        { showErrorModal && (
+            <div className="modal-overlay">
+                <div className="modal-content">
+                    <h3>Validation Errors, please check and resubmit</h3>
+                    {submissionErrors.map((error) => (
+                        <ErrorRow errorString={error}/>
+                        ))}
+                    <button
+                        type="button"
+                        onClick={()=> setShowErrorModal(false)}>OK</button>
+                </div>
+            </div>
+        )}
+        </div>
     )
+}
+
+function ErrorRow({errorString}) {
+    return (<div class="item-display"><div class="item-text">{errorString}</div></div>)
 }
 
 export default RegisterForm
