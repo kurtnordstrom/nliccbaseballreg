b35dc783210a9300e49703bcdec1d0c14cd10526
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2026-02-10 23:31:35 -0500

    Tweak cors settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,7 +79,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -148,5 +148,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+        "http://localhost:5173",
+        "http://localhost:4173",
+        "https://northstream.freeddns.org:7777"
+]
+
 CORS_ALLOWS_CREDENTIALS = True
